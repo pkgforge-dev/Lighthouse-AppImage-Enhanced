76db8a5533e262b504b1dae9c9ed5ce88c159629
Link4Electronics <link4electronics@gmail.com>
2026-08-24 18:50:07 -0300

    create patch

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -6,21 +6,35 @@ ARCH=$(uname -m)
 
 echo "Installing package dependencies..."
 echo "---------------------------------------------------------------"
-# pacman -Syu --noconfirm PACKAGESHERE
+pacman -Syu --noconfirm \
+    cmake         \
+    fmt           \
+    libzip        \
+    nlohmann-json \
+    sdl2_net      \
+    spdlog        \
+    tinyxml2
 
 echo "Installing debloated packages..."
 echo "---------------------------------------------------------------"
-get-debloated-pkgs --add-common --prefer-nano
+get-debloated-pkgs --add-common --prefer-nano libdecor-mini
 
-# Comment this out if you need an AUR package
-#make-aur-package PACKAGENAME
+make-aur-package zenity-rs-bin
 
-# If the application needs to be manually built that has to be done down here
+echo "Making stable build of Lighthouse..."
+echo "---------------------------------------------------------------"
+REPO="https://github.com/HarbourMasters/Lighthouse"
+VERSION="$(git ls-remote --tags --sort="v:refname" "$REPO" | tail -n1 | sed 's/.*\///; s/\^{}//')"
+git clone --branch "$VERSION" --single-branch --recursive --depth 1 "$REPO" ./Lighthouse
+echo "$VERSION" > ~/version
 
-# if you also have to make nightly releases check for DEVEL_RELEASE = 1
-#
-# if [ "${DEVEL_RELEASE-}" = 1 ]; then
-# 	nightly build steps
-# else
-# 	regular build steps
-# fi
+mkdir -p ./AppDir/bin
+cd ./Lighthouse
+patch -Np1 -i ../thread5-mesg-guard.patch
+
+cmake ./ -Bbuild -DCMAKE_POSITION_INDEPENDENT_CODE=ON
+cmake --build build --config Release
+cmake --build build --config Release --target GeneratePortO2R
+
+mv -v build/assets build/Lighthouse build/config.yml build/lighthouse.o2r ../AppDir/bin
+wget -O ../AppDir/bin/gamecontrollerdb.txt https://raw.githubusercontent.com/mdqinc/SDL_GameControllerDB/master/gamecontrollerdb.txt
